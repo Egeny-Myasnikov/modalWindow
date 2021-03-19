7b797db7ee39197014c568fb2afe64a82a768304
Evgeny <fl-jenya@yandex.ru>
2021-03-19 22:50:38 +0800

    test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const modal = $.modal()
+
+
+const modal = $.modal({
+    title: 'Vladilen Modal',
+    closable: true,
+    content: `
+    <h4> Какой то контент</h4>
+    <p> lorem lorem lorem lorem </p>
+    <p> lorem lorem lorem lorem </p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {text: 'Ok', type: 'btn-primary',  handler(){
+            modal.close()
+        } },
+         {text: 'Cancel', type: 'btn-danger',  handler(){
+            modal.close()
+        } },
+    ]
+})
--- plagins/modal.js
+++ plagins/modal.js
@@ -1,26 +1,51 @@
+Element.prototype.appendAfter = function(element){
+element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []){
+    if(buttons.length === 0){
+        return document.createElement('div')
+    }
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+    buttons.forEach(btn =>{
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || noop
+        wrap.appendChild($btn)
+    })
+    return wrap
+}
+
+
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
     modal.classList.add('vmodal')
     modal.insertAdjacentHTML('afterbegin', `
 
-        <div class="vmodal__overflay">
-            <div class="vmodal__window">
+        <div class="vmodal__overflay" data-close=true>
+            <div class="vmodal__window" style="width: ${options.width || DEFAULT_WIDTH}">
                 <div class="vmodal__window--header">
-                    <span class="vmodal__window--header__vmodal-title">Modal title</span>
-                    <span class="vmodal__window--header__vmodal-close">&times;</span>
+                    <span class="vmodal__window--header__vmodal-title">${options.title || 'Окно'}</span>
+                    ${options.closable ? `<span class="vmodal__window--header__vmodal-close" data-close=true>&times;</span>` : ''}
                 </div>
-                <div class="vmodal__window--body">
-                    <p>Lorem ipsum dolor sit.</p>
-                    <p>Lorem ipsum dolor sit.</p>
+                <div class="vmodal__window--body" data-content>
+                    ${options.content || ''}
                 </div>
-                <div class="vmodal__window--footer">
-                    <button>ok</button>
-                    <button>cancel</button>
-                </div>
+               
+
+
             </div>
         </div>
 
     `)
+    const footer = _createModalFooter(options.footerButtons)
+    footer.appendAfter(modal.querySelector('[data-content]'))
     document.body.appendChild(modal)
     return modal
 }
@@ -29,10 +54,13 @@ $.modal = function (options) {
     const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
     let closing = false
-
-    return {
+    let destroyed = false
+    const modal = {
         open() {
-           !closing && $modal.classList.add('open')
+            if (destroyed) {
+                return console.log('modal is destroyed');
+
+            }!closing && $modal.classList.add('open')
         },
         close() {
             closing = true
@@ -43,6 +71,28 @@ $.modal = function (options) {
                 closing = false
             }, ANIMATION_SPEED)
         },
-        destroy() {}
     }
+
+    const listener = event => {
+        if (event.target.dataset.close) {
+            modal.close()
+        }
+    }
+
+    $modal.addEventListener('click', listener)
+
+
+
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        },
+        setContent(html){
+            $modal.querySelector('[data-content]').innerHTML = html 
+
+        }
+
+    })
 }
